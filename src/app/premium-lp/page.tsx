0b17e0f84da6de9fcd85b0260c0d3cc6baa75554
Muhammad Umair <muhammad.umair@conovoinc.com>
2025-08-25 15:58:00 +0500

    feedback for premium lp

--- src/app/premium-lp/page.tsx
+++ src/app/premium-lp/page.tsx
@@ -15,10 +15,10 @@ export default function LandingPage() {
 
             <Navbar />
             <div className="flex flex-col gap-4 items-center font-black my-15 mx-5 md:mx-30">
-                <div className="text-2xl md:text-5xl">Innovative Premium</div>
-                <div className="text-3xl md:text-9xl">Crafted for the</div>
-                <div className="text-3xl md:text-9xl mb-8">Exceptional.</div>
-                <div className="md:px-80 text-center font-medium text-xl md:text-3xl">The premium range is a celebration of craftsmanship and excellence — thoughtfully created with superior ingredients and precision, offering an elevated experience in every product.</div>
+                <div className="text-xl md:text-4xl font-bold">Innovative Premium</div>
+                <div className="text-2xl md:text-6xl">Crafted for the</div>
+                <div className="text-2xl md:text-6xl mb-8">Exceptional.</div>
+                <div className="md:px-80 text-center font-medium text-base md:text-2xl">The premium range is a celebration of craftsmanship and excellence — thoughtfully created with superior ingredients and precision, offering an elevated experience in every product.</div>
             </div>
 
             <div className="flex flex-col md:flex-row gap-10 mx-15">
