3066d9cc080098c2d72217230186e3b641591307
Muhammad Umair <muhammad.umair@conovoinc.com>
2025-08-25 16:10:29 +0500

    feedback for short bread page

--- src/app/short-bread/page.tsx
+++ src/app/short-bread/page.tsx
@@ -21,23 +21,23 @@ export default function ShortBreadPage() {
         <div
           className="relative w-full bg-[#ffa372] pb-10">
               <Navbar />
-              <Image src={shortBreadBiscuit} alt='digestive-copy' className="absolute w-[35%] left-[-4rem] md:left-[-18rem] top-[13rem] md:top-[75rem] bottom-0 my-auto"/>
-              <div className="flex flex-col px-10 md:px-30 mt-12 md:mt-25">
+              <Image src={shortBreadBiscuit} alt='digestive-copy' className="hidden md:block absolute w-[35%] left-[-4rem] md:left-[-18rem] top-[40rem] bottom-0 my-auto"/>
+              <div className="flex flex-col px-10 md:px-30 mt-6 md:mt-12">
                   <Image src={shortBreadText} alt='digestive-copy' className="w-[75%] mx-auto"/>
-                  <div className="flex mt-20 md:mt-40">
+                  <div className="flex mt-10 md:mt-20">
                     <div className="md:pl-28">
-                        <div className="text-lg md:text-5xl text-center md:text-left font-semibold">Handcrafted with the finest butter and baked to perfection <span className="text-base md:text-3xl font-medium">- a cookie that speaks the language of luxury.</span></div>
+                        <div className="text-lg md:text-4xl text-center md:text-left font-semibold">Handcrafted with the finest butter and baked to perfection <span className="text-base md:text-2xl font-medium">- a cookie that speaks the language of luxury.</span></div>
                         <div className="mt-5">
                           <table className="w-full text-center border-spacing-y-2">
                             <thead>
-                              <tr className="text-base md:text-4xl border-b-1">
+                              <tr className="text-base md:text-3xl border-b-1">
                                 <td className="py-2 text-left font-semibold border-r-1">{`Packaging SKU`}</td>
                                 <td className="px-2 md:px-4 py-2 whitespace-pre-line border-r-1">{`Pack Weight\nGM`}</td>
                                 <td className="px-2 md:px-4 py-2 whitespace-pre-line border-r-1">{`Packs\nPer Box`}</td>
                                 <td className="px-2 md:px-4 py-2 whitespace-pre-line">{`Boxes\nPer Carton`}</td>
                               </tr>
                             </thead>
-                            <tbody className="text-base md:text-4xl">
+                            <tbody className="text-base md:text-3xl">
                               <tr className="border-b-1">
                                 <td className="py-3 text-left font-semibold border-r-1">Standard Pack</td>
                                 <td className="px-4 py-3 border-r-1">68</td>
