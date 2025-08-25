22ee86ba972aae6008daa25f79ad249ad0e620a7
Muhammad Umair <muhammad.umair@conovoinc.com>
2025-08-25 16:15:17 +0500

    feedback for fabulous page

--- src/app/fabulous/page.tsx
+++ src/app/fabulous/page.tsx
@@ -24,18 +24,18 @@ export default function FabulousPage() {
                   <Image src={fabulousText} alt='digestive-copy' className="w-[75%] mx-auto"/>
                   <div className="flex">
                     <div className="px-28">
-                        <div className="text-lg md:text-5xl text-center md:text-left font-semibold">Golden, chewy and loaded with gooey chocolate chips <span className="text-base md:text-3xl font-medium">{`-  these cookies are pure joy in every bite. Baked to perfection with just the right balance of crunch and softness, they're a classic made truly fabulous.`}</span></div>
+                        <div className="text-lg md:text-4xl text-center md:text-left font-semibold">Golden, chewy and loaded with gooey chocolate chips <span className="text-base md:text-2xl font-medium">{`-  these cookies are pure joy in every bite. Baked to perfection with just the right balance of crunch and softness, they're a classic made truly fabulous.`}</span></div>
                         <div className="mt-5">
                           <table className="w-full md:w-[70%] text-center border-spacing-y-2">
                             <thead>
-                              <tr className="text-base md:text-4xl border-b-1">
+                              <tr className="text-base md:text-3xl border-b-1">
                                 <td className="py-2 text-left font-semibold border-r-1">{`Packaging SKU`}</td>
                                 <td className="px-2 md:px-4 py-2 whitespace-pre-line border-r-1">{`Pack Weight\nGM`}</td>
                                 <td className="px-2 md:px-4 py-2 whitespace-pre-line border-r-1">{`Packs\nPer Box`}</td>
                                 <td className="px-2 md:px-4 py-2 whitespace-pre-line">{`Boxes\nPer Carton`}</td>
                               </tr>
                             </thead>
-                            <tbody className="text-base md:text-4xl">
+                            <tbody className="text-base md:text-3xl">
                               <tr className="border-b-1">
                                 <td className="py-3 text-left font-semibold border-r-1">Standard Pack</td>
                                 <td className="px-4 py-3 border-r-1">96</td>
