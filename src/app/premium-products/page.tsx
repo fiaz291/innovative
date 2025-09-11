62eae54ac449064fb5d510aeef6f63b1a594724b
Muhammad Umair <muhammad.umair@conovoinc.com>
2025-09-11 13:54:55 +0500

    updated copy logos

--- src/app/premium-products/page.tsx
+++ src/app/premium-products/page.tsx
@@ -3,22 +3,22 @@ import heroImg from '../../assets/premium-products-hero.webp';
 import Navbar from "@/components/Navbar";
 import Image from 'next/image';
 import { MoveRight } from 'lucide-react';
-import digestive from '../../assets/digestive.webp';
+import digestive from '../../assets/digestive.png';
 import digestiveBiscuits from '../../assets/digestive-biscuits.webp';
-import haven from '../../assets/haven.webp';
+import haven from '../../assets/haven.png';
 import havenBiscuits from '../../assets/haven-biscuits.webp';
-import biscoffi from '../../assets/biscoffi-products-page.webp';
+import biscoffi from '../../assets/biscoffi-products-page.png';
 import biscoffiBiscuits from '../../assets/biscoffi-biscuits-products-page.webp';
-import frisky from '../../assets/frisky.webp';
+import frisky from '../../assets/frisky.png';
 import friskyRollsBiscuits from '../../assets/frisky-biscuits.webp';
 import friskyWafersBiscuits from '../../assets/frisky-wafer.webp';
-import shortBread from '../../assets/shortbread.webp';
+import shortBread from '../../assets/shortbread.png';
 import shortBreadBiscuits from '../../assets/shortbread-biscuits.webp';
-import goldenLotus from '../../assets/golden-lotus-pp.webp';
+import goldenLotus from '../../assets/golden-lotus-pp.png';
 import goldenLotusBiscuit from '../../assets/golden-lotus-bisuit-pp.webp';
-import fabulous from '../../assets/fabulous-pp.webp';
+import fabulous from '../../assets/fabulous-pp.png';
 import fabulousBiscuit from '../../assets/fabulous-biscuit.webp';
-import bricklane from '../../assets/bricklane-1.webp';
+import bricklane from '../../assets/bricklane-1.png';
 import bricklaneBiscuits from '../../assets/bricklane-biscuits.webp';
 import { useRouter } from 'next/navigation';
 
@@ -45,12 +45,12 @@ export default function PremiumProductsPage() {
 
         <div className='flex flex-col m-10 md:m-20'>
             <div className='flex'>
-                <div className='flex flex-col md:w-[50%] bg-[#9f2a2a] pt-10 pb-30 justify-center text-white'>
+                <div className='flex flex-col md:w-[50%] bg-[#9f2a2a] pt-10 pb-30 justify-center text-white px-15'>
                     <Image src={digestive} alt={'digestive'} className='w-[50%]'/>
-                    <div className='text-lg md:text-2xl px-15 '>
+                    <div className='text-lg md:text-2xl mt-6'>
                         Simple Goodness, Perfectly Backed - A Wholesome Digestive Biscuit
                     </div>
-                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6 px-15' onClick={() => router.push('/digestive-premium')}>
+                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6' onClick={() => router.push('/digestive-premium')}>
                         Read More
                         <MoveRight className='w-[35px] h-[35px]' />
                     </div>
@@ -63,30 +63,30 @@ export default function PremiumProductsPage() {
                 <div className='items-center hidden md:flex w-[50%] bg-[linear-gradient(to_bottom,#f1c88e_60%,#f5f0f2_60%)]'>
                     <Image src={havenBiscuits} alt='out-products-image' className='h-[80%]'/>
                 </div>
-                <div className='flex flex-col md:w-[50%] bg-[#99693a] pt-10 pb-30 justify-center text-white'>
-                    <Image src={haven} alt={'digestive'} className='w-[50%] relative left-[-27px]'/>
-                    <div className='text-xl md:text-3xl font-black mt-6 px-15'>
+                <div className='flex flex-col md:w-[50%] bg-[#99693a] pt-10 pb-30 justify-center text-white px-15'>
+                    <Image src={haven} alt={'digestive'} className='w-[40%] relative left-[-8px]'/>
+                    <div className='text-xl md:text-3xl font-black mt-6'>
                         Cookies With Indulging Chocolate Inside
                     </div>
-                    <div className='text-lg md:text-2xl px-15 '>
+                    <div className='text-lg md:text-2xl '>
                         Gourmet on the Outside, Decadence on the Inside
                     </div>
-                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6 px-15' onClick={() => router.push('/haven')}>
+                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6' onClick={() => router.push('/haven')}>
                         Read More
                         <MoveRight className='w-[35px] h-[35px]' />
                     </div>
                 </div>
             </div>
             <div className='flex'>
-                <div className='flex flex-col md:w-[50%] bg-[#231f20] pt-10 pb-30 justify-center text-white'>
-                    <Image src={biscoffi} alt={'digestive'} className='w-[50%] relative left-[-33px]'/>
-                    <div className='text-xl md:text-3xl font-black mt-6 px-15'>
+                <div className='flex flex-col md:w-[50%] bg-[#231f20] pt-10 pb-30 justify-center text-white px-15'>
+                    <Image src={biscoffi} alt={'digestive'} className='w-[50%] relative left-[-6px]'/>
+                    <div className='text-xl md:text-3xl font-black mt-6'>
                         The Aromatic Coffee Crackers
                     </div>
-                    <div className='text-lg md:text-2xl px-15 '>
+                    <div className='text-lg md:text-2xl'>
                         Brewed to Be Eaten - Artisan Crackers Infused with Rich Coffee Notes
                     </div>
-                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6 px-15' onClick={() => router.push('/biscoffi')}>
+                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6' onClick={() => router.push('/biscoffi')}>
                         Read More
                         <MoveRight className='w-[35px] h-[35px]' />
                     </div>
@@ -99,30 +99,30 @@ export default function PremiumProductsPage() {
                 <div className='items-center hidden md:flex w-[50%] bg-[linear-gradient(to_bottom,#ffaa93_50%,#f5f0f2_50%)]'>
                     <Image src={friskyRollsBiscuits} alt='out-products-image' className='h-[80%]'/>
                 </div>
-                <div className='flex flex-col md:w-[50%] bg-[#79498c] pt-10 pb-30 justify-center text-white'>
-                    <Image src={frisky} alt={'digestive'} className='w-[50%] relative left-[-33px]'/>
-                    <div className='text-xl md:text-3xl font-black mt-6 px-15'>
+                <div className='flex flex-col md:w-[50%] bg-[#79498c] pt-10 pb-30 justify-center text-white px-15'>
+                    <Image src={frisky} alt={'digestive'} className='w-[40%] relative left-[-8px]'/>
+                    <div className='text-xl md:text-3xl font-black mt-6'>
                         Crispy Wafer Rolls Filled With Bursting Cream
                     </div>
-                    <div className='text-lg md:text-2xl px-15 '>
+                    <div className='text-lg md:text-2xl'>
                         Swirls of Delight - Delicately Rolls Wafers Filled With Lush Cream
                     </div>
-                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6 px-15' onClick={() => router.push('/frisky-rolls')}>
+                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6' onClick={() => router.push('/frisky-rolls')}>
                         Read More
                         <MoveRight className='w-[35px] h-[35px]' />
                     </div>
                 </div>
             </div>
             <div className='flex'>
-                <div className='flex flex-col md:w-[50%] bg-[#6b9ebd] pt-10 pb-30 justify-center text-white'>
-                    <Image src={frisky} alt={'digestive'} className='w-[50%] relative left-[-33px]'/>
-                    <div className='text-xl md:text-3xl font-black mt-6 px-15'>
+                <div className='flex flex-col md:w-[50%] bg-[#6b9ebd] pt-10 pb-30 justify-center text-white px-15'>
+                    <Image src={frisky} alt={'digestive'} className='w-[40%] relative left-[-8px]'/>
+                    <div className='text-xl md:text-3xl font-black mt-6'>
                         Crispy Wafer Filled With Flavoured Cream
                     </div>
-                    <div className='text-lg md:text-2xl px-15 '>
+                    <div className='text-lg md:text-2xl'>
                         Crisp Layers of Delight - Light, Flaky Wafers Filled with Smooth Cream
                     </div>
-                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6 px-15' onClick={() => router.push('/frisky-wafer')}>
+                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6' onClick={() => router.push('/frisky-wafer')}>
                         Read More
                         <MoveRight className='w-[35px] h-[35px]' />
                     </div>
@@ -135,30 +135,30 @@ export default function PremiumProductsPage() {
                 <div className='items-center hidden md:flex w-[50%] bg-[linear-gradient(to_bottom,#ffa372_70%,#f5f0f2_70%)]'>
                     <Image src={shortBreadBiscuits} alt='out-products-image' className='h-[80%]'/>
                 </div>
-                <div className='flex flex-col md:w-[50%] bg-[#231f20] pt-10 pb-30 justify-center text-white'>
-                    <Image src={shortBread} alt={'digestive'} className='w-[50%] relative left-[-20px]'/>
-                    <div className='text-xl md:text-3xl font-black mt-6 px-15'>
+                <div className='flex flex-col md:w-[50%] bg-[#231f20] pt-10 pb-30 justify-center text-white px-15'>
+                    <Image src={shortBread} alt={'digestive'} className='w-[50%] relative left-[-10px]'/>
+                    <div className='text-xl md:text-3xl font-black mt-6'>
                         Butter Cookies
                     </div>
-                    <div className='text-lg md:text-2xl px-15 '>
+                    <div className='text-lg md:text-2xl'>
                         Handcrafted with the finest butter and baked to perfection
                     </div>
-                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6 px-15' onClick={() => router.push('/short-bread')}>
+                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6' onClick={() => router.push('/short-bread')}>
                         Read More
                         <MoveRight className='w-[35px] h-[35px]' />
                     </div>
                 </div>
             </div>
             <div className='flex'>
-                <div className='flex flex-col md:w-[50%] bg-[#b54845] pt-10 pb-30 justify-center text-white'>
-                    <Image src={goldenLotus} alt={'digestive'} className='w-[50%] relative left-[-20px]'/>
-                    <div className='text-xl md:text-3xl font-black mt-6 px-15'>
+                <div className='flex flex-col md:w-[50%] bg-[#b54845] pt-10 pb-30 justify-center text-white px-15'>
+                    <Image src={goldenLotus} alt={'digestive'} className='w-[50%] relative left-[-10px]'/>
+                    <div className='text-xl md:text-3xl font-black mt-6'>
                         Golden Caramel Goodness
                     </div>
-                    <div className='text-lg md:text-2xl px-15 '>
+                    <div className='text-lg md:text-2xl'>
                         Steeped in tradition, perfected for today - premium Lotus biscuits
                     </div>
-                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6 px-15' onClick={() => router.push('/golden-lotus')}>
+                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6' onClick={() => router.push('/golden-lotus')}>
                         Read More
                         <MoveRight className='w-[35px] h-[35px]' />
                     </div>
@@ -171,30 +171,30 @@ export default function PremiumProductsPage() {
                 <div className='items-center hidden md:flex w-[50%] bg-[linear-gradient(to_bottom,#f8b145_60%,#f5f0f2_60%)]'>
                     <Image src={fabulousBiscuit} alt='out-products-image' className='h-[80%]'/>
                 </div>
-                <div className='flex flex-col md:w-[50%] bg-[#79498c] pt-10 pb-30 justify-center text-white'>
-                    <Image src={fabulous} alt={'digestive'} className='w-[50%] relative left-[-30px]'/>
-                    <div className='text-xl md:text-3xl font-black mt-6 px-15'>
+                <div className='flex flex-col md:w-[50%] bg-[#79498c] pt-10 pb-30 justify-center text-white px-15'>
+                    <Image src={fabulous} alt={'digestive'} className='w-[50%] relative left-[-10px]'/>
+                    <div className='text-xl md:text-3xl font-black mt-6'>
                         Chocolate Chip Cookies
                     </div>
-                    <div className='text-lg md:text-2xl px-15 '>
+                    <div className='text-lg md:text-2xl'>
                         Golden, chewy and loaded with gooey chocolate chips - these cookies are oure joy in every bite.
                     </div>
-                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6 px-15' onClick={() => router.push('/fabulous')}>
+                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6' onClick={() => router.push('/fabulous')}>
                         Read More
                         <MoveRight className='w-[35px] h-[35px]' />
                     </div>
                 </div>
             </div>
             <div className='flex'>
-                <div className='flex flex-col md:w-[50%] bg-[#9f2a2a] pt-10 pb-30 justify-center text-white'>
-                    <Image src={bricklane} alt={'digestive'} className='w-[50%] relative left-[-60px]'/>
-                    <div className='text-xl md:text-3xl font-black mt-6 px-15'>
+                <div className='flex flex-col md:w-[50%] bg-[#9f2a2a] pt-10 pb-30 justify-center text-white px-15'>
+                    <Image src={bricklane} alt={'digestive'} className='w-[25%] relative left-[-10px]'/>
+                    <div className='text-xl md:text-3xl font-black mt-6'>
                         Experience The Chocolate Indulgence
                     </div>
-                    <div className='text-lg md:text-2xl px-15 '>
+                    <div className='text-lg md:text-2xl'>
                         Premium taste with a purpose - ethically sourced, beautifully crafted, truly unforgettable
                     </div>
-                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6 px-15' onClick={() => router.push('/bricklane')}>
+                    <div className='cursor-pointer flex items-center gap-10 text-lg md:text-2xl font-black mt-6' onClick={() => router.push('/bricklane')}>
                         Read More
                         <MoveRight className='w-[35px] h-[35px]' />
                     </div>
