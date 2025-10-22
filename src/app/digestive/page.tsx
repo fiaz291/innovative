d6a5e8b92e7f134d2c144e19b48abf7b13d8506a
Muhammad Umair <muhammad.umair@conovoinc.com>
2025-10-22 11:23:01 +0500

    minor ui fix

--- src/app/digestive/page.tsx
+++ src/app/digestive/page.tsx
@@ -19,7 +19,7 @@ export default function DigestivePage() {
                   <Image src={digestiveLogo} alt='digestive-copy' className="w-[50%] md:w-[30%]"/>
                   <Image src={innovativeBiscuit} alt='digestive-copy' className="absolute w-[30%] md:w-[20%] right-[-3rem] md:right-[-6rem]"/>
                   <Image src={digestiveWheat} alt='digestive-copy' className="absolute w-[15%] left-[-1rem] md:left-[-3rem] top-[-5rem] md:top-[-1rem]"/>
-                  <Image src={innovativeBiscuit} alt='digestive-copy' className="absolute w-[20%] left-[-2rem] md:left-[-10rem] bottom-24 md:bottom-[-24rem]"/>
+                  <Image src={innovativeBiscuit} alt='digestive-copy' className="absolute w-[20%] left-[-3rem] md:left-[-10rem] bottom-[12rem] md:bottom-[-24rem]"/>
                   <Image src={digestiveWheat} alt='digestive-copy' className="absolute w-[15%] right-[-1rem] md:right-[-2rem] bottom-[-5rem] rotate-[150deg]"/>
                   <div className="text-5xl md:text-[8rem] md:leading-[8rem] font-black whitespace-pre-line">
                     {`Taste the\nGoodness.`}
@@ -30,11 +30,11 @@ export default function DigestivePage() {
                     <Image src={digestiveBoxWrapper} alt='digestive-box' className="w-full relative left-[-25px] md:left-[-65px]"/>
                   </div>
                     <div className="flex justify-center z-10">
-                        <div className="w-[80%] md:mt-20 md:px-20">
+                        <div className="md:w-[80%] md:mt-20 md:px-20">
                           <table className="w-full text-center border-spacing-y-2">
                             <thead>
                               <tr className="text-xs md:text-xl border-b-1">
-                                <td className="py-2 text-left font-semibold border-r-1">{`Packaging SKU`}</td>
+                                <td className="pr-2 md:pr-0 py-2 text-left font-semibold border-r-1">{`Packaging SKU`}</td>
                                 <td className="px-2 md:px-4 py-2 whitespace-pre-line border-r-1">{`Pack Weight\nGM`}</td>
                                 <td className="px-2 md:px-4 py-2 whitespace-pre-line border-r-1">{`Packs\nPer Box`}</td>
                                 <td className="px-2 md:px-4 py-2 whitespace-pre-line">{`Boxes\nPer Carton`}</td>
