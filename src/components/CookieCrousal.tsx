154155da486061afbac3a3acbec68ac8f7ffa1c6
Muhammad Umair <muhammad.umair@conovoinc.com>
2025-11-03 18:40:55 +0500

    update cookie crousal

--- src/components/CookieCrousal.tsx
+++ src/components/CookieCrousal.tsx
@@ -4,40 +4,68 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
-import biscuit1 from '../assets/packages/Digestive.png'
-import biscuit2 from '../assets/packages/Zeera.png'
-import biscuit3 from '../assets/packages/TooGud.png'
-import biscuit4 from '../assets/packages/Peanut.png'
-import biscuit5 from '../assets/packages/Snapp.png'
-import biscuit6 from '../assets/packages/Goodies.png'
-import biscuit7 from '../assets/packages/Jumbo-Junior.png'
-import biscuit8 from '../assets/packages/Butter-Crunch.png'
-import biscuit9 from '../assets/packages/Charm.png'
-import biscuit10 from '../assets/packages/Crust-Rolls.png'
-import biscuit11 from '../assets/packages/Choc-n-Chip-1.png'
-import biscuit12 from '../assets/packages/Crust-Wafer.png'
-import biscuit13 from '../assets/packages/Bitten.png'
-import biscuit14 from '../assets/packages/Choco-Rings.png'
+import havenChocolate from '../assets/slider/Haven-Chocolate.png';
+import havenCoffee from '../assets/slider/Haven-Coffee.png';
+import shortBread from '../assets/slider/ShortBread.png';
+import digestiveOriginal from '../assets/slider/Digestive-Original.png';
+import digestiveSugarFree from '../assets/slider/Digestive-Sugar-Free.png';
+import fabulous from '../assets/slider/Fabulous.png';
+import goldenLotus from '../assets/slider/Golden-Lotus.png';
+import biscoffi from '../assets/slider/Biscoffi.png';
+import friskyWaferChocolate from '../assets/slider/Frisky-Wafer-Chocolate.png';
+import friskyWaferOrgange from '../assets/slider/Frisky-Wafer-Orange.png';
+import friskyWaferStrawberry from '../assets/slider/Frisky-Wafer-Strawberry.png';
+import friskyWaferVanilla from '../assets/slider/Frisky-Wafer-Vanilla.png';
+import friskyRollsChocolate from '../assets/slider/Frisky-Rolls-Chocolate.png';
+import friskyRollsHazelnut from '../assets/slider/Frisky-Rolls-Hazelnut.png';
+import friskyRollsMilk from '../assets/slider/Frisky-Rolls-Milk.png';
+import bricklane from '../assets/slider/Bricklane.png';
+import bittens from '../assets/packages/Bitten.png';
+import digestiveRegular from '../assets/packages/Digestive.png';
+import butterCrunch from '../assets/packages/Butter-Crunch.png';
+import tooGud from '../assets/packages/TooGud.png';
+import jumboJunior from '../assets/packages/Jumbo-Junior.png';
+import goodies from '../assets/packages/Goodies.png';
+import charm from '../assets/packages/Charm.png';
+import chocNChip from '../assets/packages/Choc-n-Chip-1.png';
+import chocoRings from '../assets/packages/Choco-Rings.png';
+import crustWafer from '../assets/packages/Crust-Wafer.png'
+import crustRolls from '../assets/packages/Crust-Rolls.png'
+import { useRouter } from "next/navigation";
 
-// Replace with your actual image paths
-const cookieImages = [
-    biscuit1,
-    biscuit8,
-    biscuit13,
-    biscuit14,
-    biscuit6,
-    biscuit3,
-    biscuit2,
-    biscuit4,
-    biscuit5,
-    biscuit7,
-    biscuit9,
-    biscuit10,
-    biscuit11,
-    biscuit12,
+
+const cookies = [
+  {image: havenChocolate, route: '/haven'},
+  {image: havenCoffee, route: '/haven'},
+  {image: shortBread, route: '/short-bread'},
+  {image: digestiveOriginal, route: '/digestive-premium'},  
+  {image: digestiveSugarFree, route: '/digestive-premium'},
+  {image: fabulous, route: '/fabulous'},
+  {image: goldenLotus, route: '/golden-lotus'},
+  {image: biscoffi, route: '/biscoffi'},
+  {image: friskyWaferChocolate, route: '/frisky-wafer'},
+  {image: friskyWaferOrgange, route: '/frisky-wafer'},
+  {image: friskyWaferStrawberry, route: '/frisky-wafer'},
+  {image: friskyWaferVanilla, route: '/frisky-wafer'},
+  {image: friskyRollsChocolate, route: '/frisky-rolls'},
+  {image: friskyRollsHazelnut, route: '/frisky-rolls'},
+  {image: friskyRollsMilk, route: '/frisky-rolls'},
+  {image: bricklane, route: '/bricklane'},
+  {image: bittens, route: '/bittens'},
+  {image: digestiveRegular, route: '/digestive'},
+  {image: butterCrunch, route: '/butter-crunch'},
+  {image: tooGud, route: '/tooo-gud'},
+  {image: jumboJunior, route: '/jumbo-junior'},
+  {image: goodies, route: '/goodies'},
+  {image: charm, route: '/charm'},
+  {image: chocNChip, route: '/choc-n-chip'},
+  {image: chocoRings, route: '/choco-rings'},
+  {image: crustWafer, route: '/crust-wafer'},
+  {image: crustRolls, route: '/crust-rolls'},
 ];
 
 export default function CookieCarousel() {
+  const router = useRouter();
   return (
     <div className="w-full px-4 relative">
       <style jsx>{`
@@ -81,15 +109,16 @@ export default function CookieCarousel() {
         }}
         className="!py-4"
       >
-        {cookieImages.map((img, i) => (
+        {cookies.map((cookie, i) => (
           <SwiperSlide
             key={i}
             className="!w-auto flex justify-center items-center"
           >
             <img
-              src={img.src}
+              src={cookie.image.src}
               alt={`cookie-${i}`}
-              className="w-[100%] md:w-[100%] h-[100px] md:h-[160px] rounded-lg"
+              className="w-[100%] md:w-[100%] h-[100px] md:h-[160px] rounded-lg cursor-pointer"
+              onClick={()=>router.push(cookie.route)}
             />
           </SwiperSlide>
         ))}
